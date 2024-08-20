b2e74d1703b2398fc14c0e64789d7d911ba50fa9
Dinesh <dbhosle33@gmail.com>
2024-08-19 21:08:53 -0700

    added number of parameters print message

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from transformer import *
 device = torch.device('mps')
 torch.manual_seed(1337)
 
-
-
 model = Transformer(embd_sze=384, src_vocab_sze=64, trgt_vocab_sze=64, max_seq_len=256)
 model.to(device=device)
+print("Number of parameters: {} M".format(sum(p.numel() for p in model.parameters())/1e6))
